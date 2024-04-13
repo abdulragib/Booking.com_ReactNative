038c0ee33b93d68a2d7ec32ef17fa2c47aab40e1
Abdul Ragib <abdulragib36@gmail.com>
2024-04-13 18:37:07 +0530

    layout done

---
+++ screens/HomeScreen.js
@@ -0,0 +1,40 @@
+import { StyleSheet, Text, View } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
+
+const HomeScreen = () => {
+  const navigation = useNavigation();
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: true,
+      title: "Booking.com",
+      headerTitleStyle: {
+        fontSize: 20,
+        fontWeight: "bold",
+        color: "white",
+      },
+      headerStyle: {
+        backgroundColor: "#003580",
+        height: 110,
+      },
+      headerRight: () => (
+        <Ionicons
+          name="notifications-outline"
+          size={24}
+          color="white"
+          style={{ marginRight: 12 }}
+        />
+      ),
+    });
+  }, []);
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
