2c6d3ea9d8745fc4a7e2b6047c83d2f0d62d5749
Abdul Ragib <abdulragib36@gmail.com>
2024-04-14 11:42:48 +0530

    Dates range

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,23 @@
-import { StyleSheet, Text, View } from "react-native";
-import React, { useLayoutEffect } from "react";
+import {
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Button,
+} from "react-native";
+import React, { useLayoutEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { Ionicons } from "@expo/vector-icons";
+import Header from "../components/Header";
+import { Feather } from "@expo/vector-icons";
+import DatePicker from "react-native-date-ranges";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
+  const [selectedDates,setSelectedDates] = useState();
+
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: true,
@@ -17,6 +30,8 @@ const HomeScreen = () => {
       headerStyle: {
         backgroundColor: "#003580",
         height: 110,
+        borderBottomColor: "transparent",
+        shadowColor: "transparent",
       },
       headerRight: () => (
         <Ionicons
@@ -28,9 +43,104 @@ const HomeScreen = () => {
       ),
     });
   }, []);
+
+  const customButton = (onConfirm) => {
+    return (
+      <Button
+        onPress={onConfirm}
+        style={{
+          container: { width: "80%", marginHorizontal: "3%" },
+          text: { fontSize: 20 },
+        }}
+        primary
+        title="Submit"
+      />
+    );
+  };
+
   return (
     <View>
-      <Text>HomeScreen</Text>
+      <Header />
+      <ScrollView>
+        <View
+          style={{
+            margin: 20,
+            borderColor: "#FFC72C",
+            borderWidth: 3,
+            borderRadius: 6,
+          }}
+        >
+          {/* Destination */}
+          <Pressable
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 10,
+              paddingHorizontal: 10,
+              borderColor: "#FFC72C",
+              borderWidth: 2,
+              paddingVertical: 15,
+            }}
+          >
+            <Feather name="search" size={24} color="black" />
+            <TextInput placeholder="Enter you Destination" />
+          </Pressable>
+
+          {/* Selected Dates */}
+          <Pressable
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 10,
+              paddingHorizontal: 10,
+              borderColor: "#FFC72C",
+              borderWidth: 2,
+              paddingVertical: 15,
+            }}
+          >
+            <Feather name="calendar" size={24} color="black" />
+            <DatePicker
+              style={{
+                width: 350,
+                height: 30,
+                borderRadius: 0,
+                borderWidth: 0,
+                borderColor: "transparent",
+              }}
+              customStyles={{
+                placeholderText: {
+                  fontSize: 15,
+                  flexDirection: "row",
+                  alignItems: "center",
+                  marginRight: "auto",
+                }, // placeHolder style
+                headerStyle: {
+                  backgroundColor: "#003580",
+                },
+                contentText: {
+                  fontSize: 15,
+                  flexDirection: "row",
+                  alignItems: "center",
+                  marginRight: "auto",
+                },
+              }}
+              selectedBgColor="#0047AB"
+              customButton={(onConfirm) => customButton(onConfirm)}
+              onConfirm={(startDate,endDate)=>setSelectedDates(startDate,endDate)}
+              centerAlign
+              allowFontScaling={false}
+              placeholder={"Apr 27, 2018 → Jul 10, 2018"}
+              mode={"range"}
+            />
+          </Pressable>
+
+          {/* Rooms and Guest */}
+          <Pressable></Pressable>
+
+          {/* Search Button */}
+          <Pressable></Pressable>
+        </View>
+      </ScrollView>
     </View>
   );
 };
