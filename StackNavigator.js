cec040b8110e6eb6e2653da9f1ece34d771b152d
Abdul Ragib <abdulragib36@gmail.com>
2024-04-27 17:57:44 +0530

    confirmation screen done

--- StackNavigator.js
+++ StackNavigator.js
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
 import { AntDesign } from "@expo/vector-icons";
 import { Entypo } from "@expo/vector-icons";
@@ -9,13 +9,14 @@ import SavedScreen from "./screens/SavedScreen";
 import BookingScreen from "./screens/BookingScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 import { Ionicons } from "@expo/vector-icons";
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer } from "@react-navigation/native";
 import SearchScreen from "./screens/SearchScreen";
 import PlacesScreen from "./screens/PlacesScreen";
 import MapScreen from "./screens/MapScreen";
 import PropertyInfoScreen from "./screens/PropertyInfoScreen";
 import RoomsScreen from "./screens/RoomsScreen";
 import UserScreen from "./screens/UserScreen";
+import ConfirmationScreen from "./screens/ConfirmationScreen";
 
 const StackNavigator = () => {
   const Tab = createBottomTabNavigator();
@@ -93,13 +94,26 @@ const StackNavigator = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Main" component={BottomTabs} options={{headerShown:false}}/>
-        <Stack.Screen name="Search" component={SearchScreen} options={{headerShown:false}}/>
+        <Stack.Screen
+          name="Main"
+          component={BottomTabs}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Search"
+          component={SearchScreen}
+          options={{ headerShown: false }}
+        />
         <Stack.Screen name="Places" component={PlacesScreen} />
-        <Stack.Screen name="Map" component={MapScreen} options={{headerShown:false}}/>
-        <Stack.Screen name="Info" component={PropertyInfoScreen}/>
-        <Stack.Screen name="Rooms" component={RoomsScreen}/>
-        <Stack.Screen name="User"  component={UserScreen}/>
+        <Stack.Screen
+          name="Map"
+          component={MapScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="Info" component={PropertyInfoScreen} />
+        <Stack.Screen name="Rooms" component={RoomsScreen} />
+        <Stack.Screen name="User" component={UserScreen} />
+        <Stack.Screen name="Confirmation" component={ConfirmationScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- screens/UserScreen.js
+++ screens/UserScreen.js
@@ -1,34 +1,156 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useLayoutEffect } from 'react'
-import { useNavigation } from '@react-navigation/native';
+import {
+  Alert,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import React, { useLayoutEffect, useState } from "react";
+import { useNavigation, useRoute } from "@react-navigation/native";
 
 const UserScreen = () => {
-    const navigation=useNavigation()
-    useLayoutEffect(() => {
-        navigation.setOptions({
-          headerShown: true,
-          title: "User Details",
-          headerTitleStyle: {
-            fontSize: 20,
-            fontWeight: "bold",
-            color: "white",
-          },
-          headerStyle: {
-            backgroundColor: "#003580",
-            height: 110,
-            borderBottomColor: "transparent",
-            shadowColor: "transparent",
-          },
-          headerBackTitle: "Back",
-        });
-      }, []);
+  const navigation = useNavigation();
+  const route = useRoute();
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: true,
+      title: "User Details",
+      headerTitleStyle: {
+        fontSize: 20,
+        fontWeight: "bold",
+        color: "white",
+      },
+      headerStyle: {
+        backgroundColor: "#003580",
+        height: 110,
+        borderBottomColor: "transparent",
+        shadowColor: "transparent",
+      },
+      headerBackTitle: "Back",
+    });
+  }, []);
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNo, setPhoneNo] = useState("");
+
+  const finalStep = () => {
+    if (!firstName || !lastName || !email || !phoneNo) {
+      Alert.alert("Invalid Detials", "Please Enter All The Fields", [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel",
+        },
+        { text: "OK", onPress: () => console.log("OK Pressed") },
+      ]);
+    }
+
+    if (firstName && lastName && email && phoneNo) {
+      navigation.navigate("Confirmation", {
+        oldPrice: route.params.oldPrice,
+        newPrice: route.params.newPrice,
+        name: route.params.name,
+        children: route.params.children,
+        adults: route.params.adults,
+        rating: route.params.rating,
+        startDate: route.params.startDate,
+        endDate: route.params.endDate,
+      });
+    }
+  };
+
   return (
-    <View>
-      <Text>UserScreen</Text>
-    </View>
-  )
-}
+    <>
+      <View style={{ padding: 20 }}>
+        <View style={{ flexDirection: "column", gap: 10 }}>
+          <Text>First Name</Text>
+          <TextInput
+            value={firstName}
+            onChangeText={(text) => setFirstName(text)}
+            style={{ padding: 10, borderWidth: 1, borderColor: "gray" }}
+          />
+        </View>
 
-export default UserScreen
+        <View style={{ flexDirection: "column", gap: 10, marginTop: 15 }}>
+          <Text>Last Name</Text>
+          <TextInput
+            value={lastName}
+            onChangeText={(text) => setLastName(text)}
+            style={{ padding: 10, borderWidth: 1, borderColor: "gray" }}
+          />
+        </View>
 
-const styles = StyleSheet.create({})
+        <View style={{ flexDirection: "column", gap: 10, marginTop: 15 }}>
+          <Text>Email</Text>
+          <TextInput
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+            style={{ padding: 10, borderWidth: 1, borderColor: "gray" }}
+          />
+        </View>
+
+        <View style={{ flexDirection: "column", gap: 10, marginTop: 15 }}>
+          <Text>Phone no</Text>
+          <TextInput
+            value={phoneNo}
+            onChangeText={(text) => setPhoneNo(text)}
+            style={{ padding: 10, borderWidth: 1, borderColor: "gray" }}
+          />
+        </View>
+      </View>
+      <Pressable
+        style={{
+          backgroundColor: "white",
+          marginTop: "auto",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          padding: 10,
+          marginBottom: 25,
+        }}
+      >
+        <View>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 8,
+              marginTop: 4,
+            }}
+          >
+            <Text
+              style={{
+                color: "red",
+                fontSize: 20,
+                textDecorationLine: "line-through",
+              }}
+            >
+              {route.params.oldPrice * route.params.adults}
+            </Text>
+            <Text style={{ fontSize: 20 }}>
+              Rs {route.params.newPrice * route.params.adults}
+            </Text>
+          </View>
+          <Text>
+            You Saved {route.params.oldPrice - route.params.newPrice} rupees
+          </Text>
+        </View>
+        <Pressable
+          onPress={finalStep}
+          style={{ backgroundColor: "#007FFF", padding: 10, borderRadius: 5 }}
+        >
+          <Text style={{ textAlign: "center", fontSize: 15, color: "white" }}>
+            Final Step
+          </Text>
+        </Pressable>
+      </Pressable>
+    </>
+  );
+};
+
+export default UserScreen;
+
+const styles = StyleSheet.create({});
